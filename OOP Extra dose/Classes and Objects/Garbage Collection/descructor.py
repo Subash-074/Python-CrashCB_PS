5522b7b66a0828b9bca4f1a308244516de83ae5e
Subash <sapkotasubash784@gmail.com>
2026-03-03 19:58:21 +0100

    ss
    ss

--- OOP Extra dose/Classes and Objects/Garbage Collection/descructor.py
+++ OOP Extra dose/Classes and Objects/Garbage Collection/descructor.py
@@ -1,7 +1,53 @@
+"""import time
 class Test:
       def __init__(self):
             print("This is constructor")
       def __del__(self):
             print("This is destructor ")
 t=Test()
-print("End of Application. ")
+print("End of application")
+time.sleep(5)
+t1=t
+t2=t
+print("Test object has 3 ref now")
+del t
+print("T object is deleted ")
+
+time.sleep(5)
+del t1
+print("t1 refrence is deleted.")
+del t2
+print("t2 refrence is deleted. ")
+time.sleep(5)
+print("End of application")"""
+
+import sys
+class Test:
+      def __init__(self):
+            print("This is constructor")
+      def __del__(self):
+            print("This is destructor")
+l=[Test(), Test(), Test()]
+del l
+print("End of application")
+
+
+print(sys.getrefcount())
+
+
+
+"""
+Differences between Constructor and Distructor 
+
+Constructor -  __init__(self)
+instance variable initialiazation or default variable inside class 
+it is called as soon as object is made 
+
+
+Destruactor: __del__(self)
+clean up activities/ resource deallocation from python virtual machine 
+it is called just before Garbage collector destroys the object. 
+
+
+
+"""
---
+++ OOP Extra dose/Classes and Objects/usmemofonclasinsanothclass.py
@@ -0,0 +1,6 @@
+#Using members of one class inside another class 
+
+#There are two ways to access members of one class in another class 
+
+#1) HAS-A relation(Composition)
+#2) Is-a relation(Inheritance)
