6ad7d6b237aef96768e13f0ccda0e7c7e8e4f3ea
Subash <sapkotasubash784@gmail.com>
2026-03-05 00:16:59 +0100

    ssss

---
+++ OOP Extra dose/Classes and Objects/Inheritanceee/inheritanc.py
@@ -0,0 +1,7 @@
+#Is-a relation (Inheritance)
+#The process of creating new classes based on some existing classes is called inheritance. 
+
+#Suppose you create one small class and then create another big class which will have all the features of small class  plus extra features or functionality this is in general terms inheritance. 
+
+#
+
