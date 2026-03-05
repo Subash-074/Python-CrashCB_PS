ef83229da659e1a4e6a76a64788556dd25191314
Subash <sapkotasubash784@gmail.com>
2026-03-06 00:33:50 +0100

    ddd

--- OOP Extra dose/Classes and Objects/Inheritanceee/inheritanc.py
+++ OOP Extra dose/Classes and Objects/Inheritanceee/inheritanc.py
@@ -3,5 +3,17 @@
 
 #Suppose you create one small class and then create another big class which will have all the features of small class  plus extra features or functionality this is in general terms inheritance. 
 
-#
+class P:
+      def m(self):
+            print("This is m method of class P")
+class C(P):
+      def m1(self):
+            print("This is m1 method of class C")
+
+
+c=C()
+c.m()
+c.m1()
+
+
 
