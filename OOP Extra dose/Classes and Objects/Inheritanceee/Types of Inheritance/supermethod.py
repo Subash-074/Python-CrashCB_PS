12b4f02bba1419102fdfec5de9cdd44f307859d9
Subash <sapkotasubash784@gmail.com>
2026-03-16 23:12:09 +0100

    polymorphism intro completed

--- OOP Extra dose/Classes and Objects/Inheritanceee/Types of Inheritance/supermethod.py
+++ OOP Extra dose/Classes and Objects/Inheritanceee/Types of Inheritance/supermethod.py
@@ -66,19 +66,148 @@
 
 
 
-class A:
-      def m1(self):
-            print("A clas method ")
-class B(A):
-      def m1(self):
-            print("B clas method ")
-class C(B):
-      def m1(self):
-            print("C clas method ")
-class D(C):
-      def m1(self):
-            print("D clas method ")
-class E(D):
-      def m1(self):
-            print("E clas method ")
+# class A:
+#       def m1(self):
+#             print("A clas method ")
+# class B(A):
+#       def m1(self):
+#             print("B clas method ")
+# class C(B):
+#       def m1(self):
+#             print("C clas method ")
+# class D(C):
+#       def m1(self):
+#             print("D clas method ")
+# class E(D):
+#       def m1(self):
+#             # super().m1()# m1 method from inherited class D gets priority first 
+#             # A.m1(self) #We directly called m1 method from class A 
+#             super(B, self).m1() #we used super method and gave Class name and self parameter followed by m1 method. 
+
+#             #these are the three different methods we can use to call desired methods from any class inside method of other class. 
+#             print("E clas method ")
+
+# e=E()
+# e.m1()
       
+
+
+
+
+
+
+
+#loopholes of super method 
+#Case 1: From child class we are not allowed to access parent class instance variable by super(), compulsorarily we should use self only. use when there is class variable/static variable in parent class ===super() to access parent class form child class
+# # In a child class, parent instance variables should be accessed using self, while super() is used to access parent methods and class/static variables..
+
+
+# class P:
+#       a=10
+#       def __init__(self):
+#             self.b=20
+
+# class C(P):
+#       def m1(self):
+#             print(super().a)# this is valid 
+#             # print(super().b) this is invalid instead we should use 
+#             print(self.b)
+# c=C()
+# c.m1()
+
+
+
+
+
+
+
+
+#case 2:From a child class (constructor or instance method), we can access the parent class’s instance, class, and static methods using super()
+
+# class P:
+#       def __init__(self):
+#             print("Parent class constructor ")
+#       def m1(self):
+#             print("Parent class instance method")
+#       @classmethod
+#       def m2(cls):
+#             print("Parent class class method ")
+#       @staticmethod
+#       def m3():
+#             print("Parent class static method ")
+
+# class C(P):
+#       def __init__(self):#from the constructor of child class 
+#             super().__init__()#we called constructor of parent class P 
+#             super().m1()# we called m1 instance method of parent class P
+#             super().m2()#we called class method 
+#             super().m3()#we called static method 
+#       def m1(self): #from instance method of child class also we can call and access parent class's constructor, instance, class, and static method 
+#             super().__init__()
+#             super().m1()
+#             super().m2()
+#             super().m3()
+
+
+# c=C()
+# c.m1()
+
+
+
+
+
+
+
+#Case 3: From child class's class method, we cannot access parent class's- instance method and constructor by using super() directly(note indirectly it is possible), But,  we can access class/static method using super().
+
+# class P:
+#     def __init__(self):
+#         print("Parent class constructor ")
+#     def m1(self):
+#         print("Parent class instance method")
+#     @classmethod
+#     def m2(cls):
+#         print("Parent class class method ")
+#     @staticmethod
+#     def m3():
+#         print("Parent class static method ")
+
+# class C(P):
+#     @classmethod 
+#     def m1(cls): #inside class method of child class 
+#       #   super().__init__() this is invalid
+#       # super().m1() this is also invalid 
+#       # super.m2() valid
+#       # super.m3() valid
+#       super(C, cls).__init__(cls)#indirect way to call parent class constructor 
+#       super(C, cls).m1(cls)#indirect way to call parent class instance method 
+
+
+# c = C()
+# c.m1()
+
+
+#case 4: In child class static methods we are not allowed to use super() generally but we can super method  in a special way
+
+
+# class P:
+#     def __init__(self):
+#         print("Parent class constructor ")
+#     def m1(self):
+#         print("Parent class instance method")
+#     @classmethod
+#     def m2(cls):
+#         print("Parent class class method ")
+#     @staticmethod
+#     def m3():
+#         print("Parent class static method ")
+
+# class C(P):
+#     @staticmethod
+#     def m1(self):
+#       #   super().m1()#invalid 
+#       #   super().m3()#invalid
+#       super(C, C).m2()#indirect way 
+#       super(C, C).m2()#indirect way
+# c=C()
+# c.m1()
---
+++ OOP Extra dose/Classes and Objects/Polymorphism/polyintro.py
@@ -0,0 +1,47 @@
+# """📘 Polymorphism — Clean Notes
+# Meaning
+# - Poly = many
+# - Morphe = forms
+# - Polymorphism = One thing behaving in many forms
+# Example
+# - 5 + 5 = 10
+# - '5' + '5' = '55'
+# Same operator, different behavior → polymorphism.
+
+# 🧩 Types of Polymorphism in Python
+# 1. Duck Typing (Python Philosophy)
+# - “If it walks like a duck and quacks like a duck, it’s a duck.”
+# - Python cares about behavior, not type.
+
+# 2. Overloading
+# Python does not support traditional overloading, but we simulate it.
+# a. Operator Overloading
+# - Using special methods like __add__, __mul__, etc.
+# b. Method Overloading
+# - Python does not support it directly.
+# - Achieved using default arguments or *args.
+# c. Constructor Overloading
+# - Not supported directly.
+# - Achieved using default parameters.
+
+# 3. Overriding
+# a. Method Overriding
+# - Child class provides its own version of a parent method.
+# b. Constructor Overriding
+# - Child class defines its own __init__().
+# - Parent constructor must be called manually using super().
+
+# 🐍 Pythonic Behavior
+# Duck Typing
+# - Focus on what an object can do, not what it is.
+# EAFP (Easier to Ask Forgiveness than Permission)
+# - Try the operation.
+# - Handle exceptions if something goes wrong.
+# - Pythonic style.
+# LBYL (Look Before You Leap)
+# - Check conditions before performing an operation.
+# - Less Pythonic, more common in languages like Java/C++.
+
+
+# """
+
